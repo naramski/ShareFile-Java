52edbf898f73b106bd7da43ade3e3cee98a1ac3c
Nilesh Pawar <nilesh.pawar@citrix.com>
2014-06-16 11:15:50 -0400

    Let the app configure the api-server. Currently we need to be able to support sharefile.com, sharefiletest.com, sharefiledev.com

--- CoreSFV3SDK/src/com/sharefile/api/constants/SFSDK.java
+++ CoreSFV3SDK/src/com/sharefile/api/constants/SFSDK.java
@@ -4,7 +4,37 @@ public class SFSDK
 {
 	public static final String VERSION_FOR_QUERY_URL = "v3";
 	public static final String VERSION= "V3.1.0";
-	public static final String API_SERVER = "sf-api.com";
+	
+	public static final String API_SERVER_TEST = "sf-apitest.com";
+	public static final String API_SERVER_DEV = "sf-apidev.com";
+	public static final String API_SERVER_PRODUCTION = "sf-api.com";
+		
+	public static final String[] mApiServer = {API_SERVER_PRODUCTION, API_SERVER_DEV,API_SERVER_TEST};
+		
+	private static final int HOST_INDEX_PRODUCTION = 0;
+	private static final int HOST_INDEX_DEV 		  = 1;
+	private static final int HOST_INDEX_TEST 	  = 2;
+		
+	public static final int getHostIndex(final String hostName)
+	{
+		int index = HOST_INDEX_PRODUCTION;
+		
+		if(hostName.endsWith("dev.com"))
+		{
+			index = HOST_INDEX_DEV;
+		}
+		else if(hostName.endsWith("test.com"))
+		{
+			index = HOST_INDEX_TEST;
+		}
+		
+		return index;
+	}
+	
+	public static final String getApiServer(String hostname)
+	{
+		return mApiServer[getHostIndex(hostname)];
+	}
 	
 	public static final int INTERNAL_HTTP_ERROR = 599; //last http error code i s 505. Lets start our internal errors by leaving some difference.
 }
