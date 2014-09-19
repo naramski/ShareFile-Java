1ce806593d8018a6abaa1220ee0c393f3eca1de2
Felipe Crochik <felipe.crochik@citrix.com>
2014-09-19 15:42:58 -0400

    add error information to the exception

--- ShareFileSDKAndroid/src/com/sharefile/api/exceptions/SFV3ErrorException.java
+++ ShareFileSDKAndroid/src/com/sharefile/api/exceptions/SFV3ErrorException.java
@@ -1,8 +1,8 @@
 package com.sharefile.api.exceptions;
 
-import javax.net.ssl.HttpsURLConnection;
-
 import com.sharefile.api.SFV3Error;
+
+import javax.net.ssl.HttpsURLConnection;
 
 @SuppressWarnings("serial")
 public class SFV3ErrorException extends SFAbstractException
@@ -31,4 +31,33 @@ public class SFV3ErrorException extends SFAbstractException
 		
 		return result;
 	}
+
+    @Override
+    public String getMessage() {
+        if ( mV3Error==null ) return super.getMessage();
+
+        StringBuilder str = new StringBuilder("http:" + String.valueOf(mV3Error.httpResponseCode));
+        if ( mV3Error.code!=null && !mV3Error.code.isEmpty()) {
+            str.append("; code:");
+            str.append(mV3Error.code);
+        }
+
+        if ( mV3Error.mExtraInfo!=null && !mV3Error.mExtraInfo.isEmpty() ) {
+            str.append("; extra:");
+            str.append(mV3Error.mExtraInfo);
+        }
+
+        if ( mV3Error.message!=null ) {
+            if ( mV3Error.message.lang!=null && !mV3Error.message.lang.isEmpty() ) {
+                str.append("; lang:");
+                str.append(mV3Error.message.lang);
+            }
+            if ( mV3Error.message.value!=null && !mV3Error.message.value.isEmpty() ) {
+                str.append("; value:");
+                str.append(mV3Error.message.value);
+            }
+        }
+
+        return str.toString();
+    }
 }
