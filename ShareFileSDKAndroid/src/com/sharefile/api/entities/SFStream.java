83dc4c4046d12b7dbe716e4b00aaeb1a195adff2
Nilesh Pawar <nilesh.pawar@citrix.com>
2014-10-02 13:44:56 -0400

    Remove the old SDK

--- ShareFileSDKAndroid/src/com/sharefile/api/entities/SFStream.java
+++
@@ -1,8 +0,0 @@
-package com.sharefile.api.entities;
-
-import com.sharefile.api.models.SFODataObject;
-
-public class SFStream extends SFODataObject 
-{
-
-}
--- ShareFileSDKAndroid/src/com/sharefile/api/exceptions/ReAuthException.java
+++
@@ -1,9 +0,0 @@
-package com.sharefile.api.exceptions;
-
-import com.sharefile.api.SFV3Error;
-
-public class ReAuthException extends SFV3ErrorException{
-    public ReAuthException(SFV3Error error) {
-        super(error);
-    }
-}
